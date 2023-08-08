3fd7107854818855e8753044833ebda7337f7161
LeonardoSya <2246866774@qq.com>
2023-08-08 22:38:51 +0800

    git push

--- 43.Module模块.js
+++ 43.Module模块.js
@@ -26,5 +26,21 @@ import customName from 'xxx.js';   // 注意，这时文件名不需要加大括
 customName();  // foo
 // 上面代码的Import命令，可以用任意名称指向指定默认输出的模块，这时候就不需要知道原模块输出的函数名。
 
-// 
+// 浏览器允许脚本异步加载
+/*
+<script src="path/to/myModule.js" defer></script>
+<script src="path/to/myModule.js" async></script>
+*/
+//  defer是“渲染完再执行(DOM结构完全生成)”，async是“下载完就执行”
 
+// 浏览器加载es6模块，需要加入 type="module" 属性
+<script type="module" src="./foo.js"></script>   //  由于type属性设为module，所以浏览器知道这是一个 ES6 模块。
+
+
+//  Node.js 要求 ES6 模块采用.mjs后缀文件名，并且默认启用严格模式 不必再每个模块文件顶部指定 use strict
+// 也就是说，只要脚本文件里面使用import或者export命令，那么就必须采用.mjs后缀名
+
+// 如果不希望将后缀名改成.mjs，可以在项目的package.json文件中，指定type字段为module。
+// {
+//     "type": "module"
+// }
